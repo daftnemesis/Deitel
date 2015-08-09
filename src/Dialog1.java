9c506c6a774a8d714d7b98534098bcf9436f2c6f
Jose Lozano <XxdaftnemesisixX@gmail.com>
2015-08-09 16:57:17 -0500

    Comiting

--- src/Dialog1.java
+++ src/Dialog1.java
@@ -1,8 +1,13 @@
 //Using JOptionPane to display multiple lines in a box.
+
 import javax.swing.JOptionPane;
 
 public class Dialog1 {
 
+    public static void main(String[] args){
 
+        JOptionPane.showMessageDialog(null, "Welcome to Java");
+
+    }
 
 }
