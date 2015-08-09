757a0dac767deb9d0a486dceb820baeffb6f9186
Jose Lozano <XxdaftnemesisixX@gmail.com>
2015-08-09 11:14:17 -0500

    testing

--- src/Account.java
+++ src/Account.java
@@ -2,7 +2,12 @@ public class Account {
 
     private String name;
 
+    public Account(String name) {
+        this.name = name;
+    }
+
     public void setName(String name){
+
         this.name = name;
     }
 
--- src/AccountTests.java
+++ src/AccountTests.java
@@ -1,23 +1,18 @@
-import java.util.Scanner;
+//Program that uses the Account classes for testing it
+//Uses the classes constructor to initialize the name instance
+//variable at the time each account object is created
 
 public class AccountTests {
 
     public static void main(String[] args){
 
-        Scanner input = new Scanner(System.in);
+        // create two Account objects
+        Account account1 = new Account("Jane Green");
+        Account account2 = new Account("John Blue");
 
-        Account myAccount = new Account();
-
-        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
-
-        System.out.println("Please enter the name: ");
-        String theName = input.nextLine();
-
-        myAccount.setName(theName);
-        System.out.println();
-
-        System.out.printf("Name in object myAccount is: %n%s%n",
-                myAccount.getName());
+        //display initial value of name ofr each account
+        System.out.printf("account1 name is: %s%n", account1.getName());
+        System.out.printf("account2 name is: %s%n", account2.getName());
 
     }
 }
